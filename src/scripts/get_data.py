b7281b2758d572b946af8fb36fbee1e0f75e6d6e
rubgarcia97 <rubengarciaolivas@gmail.com>
2023-10-08 11:02:54 +0200

    get tables

--- src/scripts/get_data.py
+++ src/scripts/get_data.py
@@ -1,3 +1,5 @@
+from fastf1.ergast import Ergast
+
 import fastf1 as f1
 import pandas as pd
 import datetime as dt
@@ -5,19 +7,14 @@ import datetime as dt
 class GetTables():
 
     def __init__(self):
-        pass
-
-    def events1(self):
-        events = f1.get_event(year=2022,gp=7,backend='ergast')
         
-        events_df = pd.DataFrame(data=[events.values], columns=events.index)
-        print(events_df)
+        self.__ergast = Ergast()
 
 
     def events(self):
 
         data=[]
-        for year in range(2020,dt.date.today().year,1):
+        for year in range(1950,dt.date.today().year,1):
 
             try:
                 for race in range(1,100):
@@ -28,8 +25,17 @@ class GetTables():
                 continue
 
         dataframe = pd.concat(data,axis=1).T.reset_index(drop=True)
-        dataframe["key"] = dataframe['EventDate'].year
-        print(dataframe.info())
+        dataframe['key'] = dataframe['RoundNumber'].astype(str) + dataframe['EventDate'].dt.year.astype(str)
+
+        print(dataframe)
+
+
+    def drivers(self):
+        
+        dataframe = self.__ergast.get_driver_info(season=2006)
+        print(dataframe)
+
+
 
 if __name__=='__main__':
-    GetTables().events()
+    GetTables().drivers()
